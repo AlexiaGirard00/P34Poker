02ace4349cfd2fe1cdbd87eec09e769b1bb439db
Alexia Girard <girardalexia00@gmail.com>
2023-05-15 13:57:40 -0400

    Add project files.

---
+++ P34_TP2_2210075/Form1.Designer.vb
@@ -0,0 +1,318 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.pb1 = New System.Windows.Forms.PictureBox()
+        Me.gbCartes = New System.Windows.Forms.GroupBox()
+        Me.pb2 = New System.Windows.Forms.PictureBox()
+        Me.pb4 = New System.Windows.Forms.PictureBox()
+        Me.pb5 = New System.Windows.Forms.PictureBox()
+        Me.pb3 = New System.Windows.Forms.PictureBox()
+        Me.btnContinuer = New System.Windows.Forms.Button()
+        Me.chkCarte5 = New System.Windows.Forms.CheckBox()
+        Me.chkCarte4 = New System.Windows.Forms.CheckBox()
+        Me.chkCarte3 = New System.Windows.Forms.CheckBox()
+        Me.chkCarte2 = New System.Windows.Forms.CheckBox()
+        Me.chkCarte1 = New System.Windows.Forms.CheckBox()
+        Me.gbInfoJeux = New System.Windows.Forms.GroupBox()
+        Me.numMise = New System.Windows.Forms.NumericUpDown()
+        Me.numCredit = New System.Windows.Forms.NumericUpDown()
+        Me.txtGain = New System.Windows.Forms.TextBox()
+        Me.lblGain = New System.Windows.Forms.Label()
+        Me.btnQuitter = New System.Windows.Forms.Button()
+        Me.btnJouer = New System.Windows.Forms.Button()
+        Me.lblMise = New System.Windows.Forms.Label()
+        Me.lblCredit = New System.Windows.Forms.Label()
+        CType(Me.pb1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.gbCartes.SuspendLayout()
+        CType(Me.pb2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.gbInfoJeux.SuspendLayout()
+        CType(Me.numMise, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.numCredit, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pb1
+        '
+        Me.pb1.Location = New System.Drawing.Point(18, 47)
+        Me.pb1.Name = "pb1"
+        Me.pb1.Size = New System.Drawing.Size(169, 250)
+        Me.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pb1.TabIndex = 0
+        Me.pb1.TabStop = False
+        '
+        'gbCartes
+        '
+        Me.gbCartes.Controls.Add(Me.pb2)
+        Me.gbCartes.Controls.Add(Me.pb4)
+        Me.gbCartes.Controls.Add(Me.pb5)
+        Me.gbCartes.Controls.Add(Me.pb3)
+        Me.gbCartes.Controls.Add(Me.btnContinuer)
+        Me.gbCartes.Controls.Add(Me.chkCarte5)
+        Me.gbCartes.Controls.Add(Me.chkCarte4)
+        Me.gbCartes.Controls.Add(Me.chkCarte3)
+        Me.gbCartes.Controls.Add(Me.chkCarte2)
+        Me.gbCartes.Controls.Add(Me.chkCarte1)
+        Me.gbCartes.Controls.Add(Me.pb1)
+        Me.gbCartes.Location = New System.Drawing.Point(12, 21)
+        Me.gbCartes.Name = "gbCartes"
+        Me.gbCartes.Size = New System.Drawing.Size(1059, 633)
+        Me.gbCartes.TabIndex = 1
+        Me.gbCartes.TabStop = False
+        '
+        'pb2
+        '
+        Me.pb2.Location = New System.Drawing.Point(230, 47)
+        Me.pb2.Name = "pb2"
+        Me.pb2.Size = New System.Drawing.Size(169, 250)
+        Me.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pb2.TabIndex = 14
+        Me.pb2.TabStop = False
+        '
+        'pb4
+        '
+        Me.pb4.Location = New System.Drawing.Point(653, 47)
+        Me.pb4.Name = "pb4"
+        Me.pb4.Size = New System.Drawing.Size(169, 250)
+        Me.pb4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pb4.TabIndex = 13
+        Me.pb4.TabStop = False
+        '
+        'pb5
+        '
+        Me.pb5.Location = New System.Drawing.Point(868, 47)
+        Me.pb5.Name = "pb5"
+        Me.pb5.Size = New System.Drawing.Size(169, 250)
+        Me.pb5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pb5.TabIndex = 12
+        Me.pb5.TabStop = False
+        '
+        'pb3
+        '
+        Me.pb3.Location = New System.Drawing.Point(444, 47)
+        Me.pb3.Name = "pb3"
+        Me.pb3.Size = New System.Drawing.Size(169, 250)
+        Me.pb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pb3.TabIndex = 11
+        Me.pb3.TabStop = False
+        '
+        'btnContinuer
+        '
+        Me.btnContinuer.Location = New System.Drawing.Point(367, 440)
+        Me.btnContinuer.Name = "btnContinuer"
+        Me.btnContinuer.Size = New System.Drawing.Size(310, 96)
+        Me.btnContinuer.TabIndex = 10
+        Me.btnContinuer.Text = "Continuer"
+        Me.btnContinuer.UseVisualStyleBackColor = True
+        '
+        'chkCarte5
+        '
+        Me.chkCarte5.AutoSize = True
+        Me.chkCarte5.Location = New System.Drawing.Point(893, 318)
+        Me.chkCarte5.Name = "chkCarte5"
+        Me.chkCarte5.Size = New System.Drawing.Size(117, 36)
+        Me.chkCarte5.TabIndex = 9
+        Me.chkCarte5.Text = "Garder"
+        Me.chkCarte5.UseVisualStyleBackColor = True
+        '
+        'chkCarte4
+        '
+        Me.chkCarte4.AutoSize = True
+        Me.chkCarte4.Location = New System.Drawing.Point(680, 318)
+        Me.chkCarte4.Name = "chkCarte4"
+        Me.chkCarte4.Size = New System.Drawing.Size(117, 36)
+        Me.chkCarte4.TabIndex = 8
+        Me.chkCarte4.Text = "Garder"
+        Me.chkCarte4.UseVisualStyleBackColor = True
+        '
+        'chkCarte3
+        '
+        Me.chkCarte3.AutoSize = True
+        Me.chkCarte3.Location = New System.Drawing.Point(461, 318)
+        Me.chkCarte3.Name = "chkCarte3"
+        Me.chkCarte3.Size = New System.Drawing.Size(117, 36)
+        Me.chkCarte3.TabIndex = 7
+        Me.chkCarte3.Text = "Garder"
+        Me.chkCarte3.UseVisualStyleBackColor = True
+        '
+        'chkCarte2
+        '
+        Me.chkCarte2.AutoSize = True
+        Me.chkCarte2.Location = New System.Drawing.Point(257, 318)
+        Me.chkCarte2.Name = "chkCarte2"
+        Me.chkCarte2.Size = New System.Drawing.Size(117, 36)
+        Me.chkCarte2.TabIndex = 6
+        Me.chkCarte2.Text = "Garder"
+        Me.chkCarte2.UseVisualStyleBackColor = True
+        '
+        'chkCarte1
+        '
+        Me.chkCarte1.AutoSize = True
+        Me.chkCarte1.Location = New System.Drawing.Point(40, 318)
+        Me.chkCarte1.Name = "chkCarte1"
+        Me.chkCarte1.Size = New System.Drawing.Size(117, 36)
+        Me.chkCarte1.TabIndex = 5
+        Me.chkCarte1.Text = "Garder"
+        Me.chkCarte1.UseVisualStyleBackColor = True
+        '
+        'gbInfoJeux
+        '
+        Me.gbInfoJeux.Controls.Add(Me.numMise)
+        Me.gbInfoJeux.Controls.Add(Me.numCredit)
+        Me.gbInfoJeux.Controls.Add(Me.txtGain)
+        Me.gbInfoJeux.Controls.Add(Me.lblGain)
+        Me.gbInfoJeux.Controls.Add(Me.btnQuitter)
+        Me.gbInfoJeux.Controls.Add(Me.btnJouer)
+        Me.gbInfoJeux.Controls.Add(Me.lblMise)
+        Me.gbInfoJeux.Controls.Add(Me.lblCredit)
+        Me.gbInfoJeux.Location = New System.Drawing.Point(1098, 21)
+        Me.gbInfoJeux.Name = "gbInfoJeux"
+        Me.gbInfoJeux.Size = New System.Drawing.Size(448, 633)
+        Me.gbInfoJeux.TabIndex = 2
+        Me.gbInfoJeux.TabStop = False
+        '
+        'numMise
+        '
+        Me.numMise.Location = New System.Drawing.Point(131, 121)
+        Me.numMise.Name = "numMise"
+        Me.numMise.Size = New System.Drawing.Size(142, 39)
+        Me.numMise.TabIndex = 9
+        Me.numMise.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'numCredit
+        '
+        Me.numCredit.Location = New System.Drawing.Point(131, 45)
+        Me.numCredit.Name = "numCredit"
+        Me.numCredit.Size = New System.Drawing.Size(142, 39)
+        Me.numCredit.TabIndex = 8
+        Me.numCredit.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'txtGain
+        '
+        Me.txtGain.Location = New System.Drawing.Point(131, 497)
+        Me.txtGain.Name = "txtGain"
+        Me.txtGain.Size = New System.Drawing.Size(164, 39)
+        Me.txtGain.TabIndex = 7
+        Me.txtGain.Text = "0"
+        Me.txtGain.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lblGain
+        '
+        Me.lblGain.AutoSize = True
+        Me.lblGain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGain.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.lblGain.Location = New System.Drawing.Point(34, 502)
+        Me.lblGain.Name = "lblGain"
+        Me.lblGain.Size = New System.Drawing.Size(64, 34)
+        Me.lblGain.TabIndex = 6
+        Me.lblGain.Text = "Gain"
+        '
+        'btnQuitter
+        '
+        Me.btnQuitter.Location = New System.Drawing.Point(256, 271)
+        Me.btnQuitter.Name = "btnQuitter"
+        Me.btnQuitter.Size = New System.Drawing.Size(148, 83)
+        Me.btnQuitter.TabIndex = 5
+        Me.btnQuitter.Text = "Quitter"
+        Me.btnQuitter.UseVisualStyleBackColor = True
+        '
+        'btnJouer
+        '
+        Me.btnJouer.Location = New System.Drawing.Point(75, 271)
+        Me.btnJouer.Name = "btnJouer"
+        Me.btnJouer.Size = New System.Drawing.Size(148, 83)
+        Me.btnJouer.TabIndex = 4
+        Me.btnJouer.Text = "Jouer"
+        Me.btnJouer.UseVisualStyleBackColor = True
+        '
+        'lblMise
+        '
+        Me.lblMise.AutoSize = True
+        Me.lblMise.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblMise.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.lblMise.Location = New System.Drawing.Point(34, 126)
+        Me.lblMise.Name = "lblMise"
+        Me.lblMise.Size = New System.Drawing.Size(67, 34)
+        Me.lblMise.TabIndex = 1
+        Me.lblMise.Text = "Mise"
+        '
+        'lblCredit
+        '
+        Me.lblCredit.AutoSize = True
+        Me.lblCredit.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblCredit.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.lblCredit.Location = New System.Drawing.Point(34, 47)
+        Me.lblCredit.Name = "lblCredit"
+        Me.lblCredit.Size = New System.Drawing.Size(80, 34)
+        Me.lblCredit.TabIndex = 0
+        Me.lblCredit.Text = "Crédit"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 32.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1573, 806)
+        Me.Controls.Add(Me.gbInfoJeux)
+        Me.Controls.Add(Me.gbCartes)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.pb1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.gbCartes.ResumeLayout(False)
+        Me.gbCartes.PerformLayout()
+        CType(Me.pb2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.gbInfoJeux.ResumeLayout(False)
+        Me.gbInfoJeux.PerformLayout()
+        CType(Me.numMise, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.numCredit, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pb1 As PictureBox
+    Friend WithEvents gbCartes As GroupBox
+    Friend WithEvents chkCarte5 As CheckBox
+    Friend WithEvents chkCarte4 As CheckBox
+    Friend WithEvents chkCarte3 As CheckBox
+    Friend WithEvents chkCarte2 As CheckBox
+    Friend WithEvents chkCarte1 As CheckBox
+    Friend WithEvents gbInfoJeux As GroupBox
+    Friend WithEvents btnContinuer As Button
+    Friend WithEvents txtGain As TextBox
+    Friend WithEvents lblGain As Label
+    Friend WithEvents btnQuitter As Button
+    Friend WithEvents btnJouer As Button
+    Friend WithEvents lblMise As Label
+    Friend WithEvents lblCredit As Label
+    Friend WithEvents pb2 As PictureBox
+    Friend WithEvents pb4 As PictureBox
+    Friend WithEvents pb5 As PictureBox
+    Friend WithEvents pb3 As PictureBox
+    Friend WithEvents numMise As NumericUpDown
+    Friend WithEvents numCredit As NumericUpDown
+End Class
---
+++ P34_TP2_2210075/Form1.vb
@@ -0,0 +1,331 @@
+﻿Public Class Form1
+    Dim ResourceSet As Resources.ResourceSet = My.Resources.ResourceManager.GetResourceSet(Globalization.CultureInfo.CurrentCulture, True, True)
+
+    Dim listImages As New List(Of Image)
+    Dim listNamesImages As New List(Of String)
+    Dim selectedCards(4) As String
+
+    Dim listPair As New List(Of Integer)
+    Dim pairCount As Integer
+
+    Dim gainTotal As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        gbCartes.Enabled = False
+        txtGain.ReadOnly = True
+
+        numMise.Maximum = 10
+
+        For Each Dict As DictionaryEntry In ResourceSet.OfType(Of Object)
+            If TypeOf Dict.Value Is Image Then
+                listImages.Add(Dict.Value)
+                listNamesImages.Add(Dict.Key)
+            End If
+        Next
+
+        MessageBox.Show("Veuillez entrer le nombre de crédit puis appuyer sur jouer." & vbCrLf & vbCrLf & "Bonne chance !", "Poker")
+
+    End Sub
+
+    Private Sub btnJouer_Click(sender As Object, e As EventArgs) Handles btnJouer.Click
+
+        If numCredit.Value <> 0 Then
+            btnJouer.Enabled = True
+            If numMise.Value <> 0 Then
+                numCredit.Enabled = False
+                gbCartes.Enabled = True
+                btnJouer.Enabled = False
+
+                StartingHand()
+            Else
+                MessageBox.Show("Vous devez miser au minimum 1", "Mise insuffisante")
+            End If
+        Else
+            MessageBox.Show("Vous devez entrer au minimum 1 crédit.", "Crédit insuffisant")
+        End If
+
+    End Sub
+
+    Private Sub btnContinuer_Click(sender As Object, e As EventArgs) Handles btnContinuer.Click
+
+        numMise.Enabled = True
+
+        SecondHand(pb1, chkCarte1, 0)
+        SecondHand(pb2, chkCarte2, 1)
+        SecondHand(pb3, chkCarte3, 2)
+        SecondHand(pb4, chkCarte4, 3)
+        SecondHand(pb5, chkCarte5, 4)
+
+        Dim result As DialogResult
+        Dim resultCredit As DialogResult
+        Dim inputCredit As String = ""
+        Dim valide = False
+
+        If numMise.Value <> 0 Then
+            btnContinuer.Enabled = False
+            result = MessageBox.Show(CheckHands(selectedCards), "Resultat", MessageBoxButtons.YesNo)
+            If result = DialogResult.Yes Then
+                If numCredit.Value <> 0 Then
+                    StartingHand()
+                    For Each ctrl As CheckBox In gbCartes.Controls.OfType(Of CheckBox)
+                        ctrl.Checked = False
+                    Next
+                    btnContinuer.Enabled = True
+                Else
+                    resultCredit = MessageBox.Show("Vous n'avez plus de crédit." & vbCrLf & "Désirez-vous en ajouter ?", "Crédit insuffisant", MessageBoxButtons.YesNo)
+                    If resultCredit = DialogResult.Yes Then
+                        Do
+                            inputCredit = InputBox("Entrer le nombre de crédit.")
+                            If IsNumeric(inputCredit) And inputCredit <> 0 Then
+                                valide = True
+                                numCredit.Value += CInt(inputCredit)
+                                StartingHand()
+                                btnContinuer.Enabled = True
+                            Else
+                                MessageBox.Show("Vous devez entrer un nombre supérieur à 0.", "Invalide")
+                            End If
+                        Loop While Not valide
+                    Else
+                        QuitterJeu()
+                    End If
+                End If
+            Else
+                QuitterJeu()
+            End If
+        Else
+            MessageBox.Show("La mise minimum est de 1.", "Mise minimum")
+        End If
+
+    End Sub
+
+    Private Sub btnQuitter_Click(sender As Object, e As EventArgs) Handles btnQuitter.Click
+        Dim result As DialogResult
+        result = MessageBox.Show("Désirez-vous vraiment quitter le jeu ?", "Quitter ?", MessageBoxButtons.YesNo)
+
+        If result = DialogResult.Yes Then
+            QuitterJeu()
+        End If
+
+    End Sub
+
+    Private Sub QuitterJeu()
+        MessageBox.Show("Voici vos gains: " & gainTotal & ".", "Aurevoir !")
+        Close()
+    End Sub
+
+    Private Function RndIndex() As Integer
+        Dim rndNumber As New Random
+        Dim i As Integer = rndNumber.Next(0, listImages.Count)
+        Return i
+
+    End Function
+
+    Private Function CheckBoxeCheck() As Boolean
+        Dim count As Integer
+
+        For Each Control In Me.Controls
+            If TypeOf Control Is CheckBox Then
+                If CType(Control, CheckBox).Checked Then
+                    count += 1
+                End If
+            End If
+        Next
+
+        If count > 0 Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Function CheckHands(cards() As String)
+        Dim handResult As String
+        Dim suit(4) As String
+        Dim face(4) As String
+        Dim intFace(4) As Integer
+
+        Dim royal As Boolean = False
+
+        Dim fullHouseValide As Boolean = False
+        Dim fullHouseList As New List(Of Integer)
+
+        Dim flushCount As Integer = 0
+        Dim straightCount As Integer = 0
+
+        Dim mise As Integer = numMise.Value
+
+        Dim gain As Integer = 0
+
+        'Séparer les couleurs et les valeurs
+
+        'couleur
+        For i As Integer = 0 To selectedCards.Length - 1
+            suit(i) = selectedCards(i)
+            face(i) = selectedCards(i)
+        Next
+
+        For i As Integer = 0 To suit.Length - 1
+            suit(i) = suit(i).Substring(1, 1)
+        Next
+
+        'Valeur & enlever la parti suit
+        For i As Integer = 0 To face.Length - 1
+            Select Case face(i).Substring(0, 1)
+                Case "t"
+                    face(i) = "10"
+                Case "j"
+                    face(i) = "11"
+                Case "q"
+                    face(i) = "12"
+                Case "k"
+                    face(i) = "13"
+                Case "a"
+                    face(i) = "14"
+                Case Else
+                    face(i) = face(i).Substring(0, 1)
+            End Select
+        Next
+
+        'remplir face()
+
+        For i As Integer = 0 To face.Length - 1
+            intFace(i) = face(i)
+        Next
+
+        Array.Sort(intFace)
+
+
+        If intFace(0) = 2 And intFace(4) = 14 Then
+            intFace(4) = 1
+        ElseIf intFace(4) = 14 Then
+            royal = True
+        End If
+
+        Array.Sort(intFace)
+
+        'Check flush
+        For i As Integer = 0 To suit.Length - 2
+            If suit(i) = suit(i + 1) Then
+                flushCount += 1
+            End If
+        Next
+
+        'Check straight
+        For i As Integer = 0 To intFace.Length - 2
+            If intFace(i) + 1 = intFace(i + 1) Then
+                straightCount += 1
+            End If
+        Next
+
+        'check pair
+        listPair.Clear()
+        pairCount = 0
+
+        CheckPair(face, 0)
+        CheckPair(face, 1)
+        CheckPair(face, 2)
+
+        If face(3) = face(4) Then
+            listPair.Add(face(3))
+            pairCount += 1
+        End If
+
+        'Check Full house
+        For i As Integer = 0 To face.Length - 1
+            If listPair.Contains(face(i)) Then
+            Else
+                fullHouseList.Add(face(i))
+            End If
+        Next
+
+        If fullHouseList.Count = 0 Then
+            fullHouseValide = True
+        End If
+
+
+        If flushCount = 4 And straightCount = 4 And royal Then
+            gain = mise * 100
+            handResult = "Royal flush ! Vous avez fait un gain de " & gain & "." & vbCrLf & "Désirez vous continuer à jouer ?"
+        ElseIf flushCount = 4 And straightCount = 4 Then
+            gain = mise * 50
+            handResult = "Straight flush ! Vous avez fait un gain de " & gain & "." & vbCrLf & "Désirez vous continuer à jouer ?"
+        ElseIf straightCount = 4 Then
+            gain = mise * 10
+            handResult = "Straight ! Vous avez fait un gain de " & gain & "." & vbCrLf & "Désirez vous continuer à jouer ?"
+        ElseIf flushCount = 4 Then
+            gain = mise * 15
+            handResult = "Flush ! Vous avez fait un gain de " & gain & "." & vbCrLf & "Désirez vous continuer à jouer ?"
+        ElseIf pairCount = 6 Then
+            gain = mise * 25
+            handResult = "Four of kind! Vous avez fait un gain de " & gain & "." & vbCrLf & "Désirez vous continuer à jouer ?"
+        ElseIf pairCount = 4 And fullHouseValide Then
+            gain = mise * 20
+            handResult = "Full house ! Vous avez fait un gain de " & gain & "." & vbCrLf & "Désirez vous continuer à jouer ?"
+        ElseIf pairCount = 3 Then
+            gain = mise * 5
+            handResult = ("Three of kind! Vous avez fait un gain de " & gain) & "." & vbCrLf & "Désirez vous continuer à jouer ?"
+        ElseIf pairCount = 1 Then
+            gain = mise * 1
+            handResult = ("Pair ! Vous avez fait un gain de " & gain) & "." & vbCrLf & "Désirez vous continuer à jouer ?"
+        ElseIf pairCount = 2 Then
+            gain = mise * 3
+            handResult = ("Two pair ! Vous avez fait un gain de " & gain) & "." & vbCrLf & "Désirez vous continuer à jouer ?"
+        Else
+            handResult = ("Aucun gain") & "." & vbCrLf & "Désirez-vous continuer à jouer ?"
+        End If
+
+        gainTotal += gain
+        txtGain.Text = gainTotal.ToString()
+        numCredit.Value += gain
+
+        Return handResult
+    End Function
+
+    Private Sub StartingHand()
+        Dim i As Integer
+
+        numCredit.Value -= 1
+
+        i = RndIndex()
+        pb1.Image = listImages(i)
+        pb1.Tag = listNamesImages(i)
+
+        i = RndIndex()
+        pb2.Image = listImages(i)
+        pb2.Tag = listNamesImages(i)
+
+        i = RndIndex()
+        pb3.Image = listImages(i)
+        pb3.Tag = listNamesImages(i)
+
+        i = RndIndex()
+        pb4.Image = listImages(i)
+        pb4.Tag = listNamesImages(i)
+
+        i = RndIndex()
+        pb5.Image = listImages(i)
+        pb5.Tag = listNamesImages(i)
+    End Sub
+
+    Private Sub SecondHand(pb As PictureBox, chkCarte As CheckBox, j As Integer)
+        Dim i As Integer
+
+        If Not chkCarte.Checked Then
+            i = RndIndex()
+            pb.Image = listImages(i)
+            pb.Tag = listNamesImages(i)
+        End If
+
+        selectedCards(j) = (pb.Tag).ToString.Substring(0, 2)
+    End Sub
+
+    Private Sub CheckPair(face() As String, j As Integer)
+
+        For i As Integer = j To face.Length - 2
+            If face(j) = face(i + 1) Then
+                listPair.Add(face(j))
+                pairCount += 1
+            End If
+        Next
+    End Sub
+End Class
